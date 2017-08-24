4591351f54dcdf256df24cee8810f6a369d06393
manu <arslu@hotmail.fr>
2017-08-24 17:56:11 +0200

    Initial commit

---
+++ myblogvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\manu\desktop\programs-python\project_3._web_applications\myblog\myblogvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
